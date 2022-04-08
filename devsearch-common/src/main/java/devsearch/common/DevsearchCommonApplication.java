1968f9985ba0ae3cdf0b2910f7ed802af3440f58
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-04-08 23:47:09 +0300

    Initial commit

---
+++ devsearch-common/src/main/java/devsearch/common/DevsearchCommonApplication.java
@@ -0,0 +1,13 @@
+package devsearch.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsearchCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsearchCommonApplication.class, args);
+	}
+
+}
---
+++ devsearch-common/src/test/java/devsearch/common/DevsearchCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package devsearch.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsearchCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
